e27b80f60d4f3e63120fd2a4ca0ca42d247ad672
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-09-27 13:15:02 +0300

    update

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var randomNumber1 = Math.floor(Math.random()*6 + 1);
 
 var randomImageLeft = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageLeft);
+document.querySelectorAll("img")[0].setAttribute("src", randomImageLeft);
 
 /* Random Dicee Right */
 
@@ -14,9 +12,7 @@ var randomNumber2 = Math.floor(Math.random()*6 + 1);
 
 var randomImageRight = "images/dice" + randomNumber2 + ".png";
 
-var image2 = document.querySelectorAll("img")[1];
-
-image2.setAttribute("src", randomImageRight);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageRight);
 
 
 if (randomNumber1 > randomNumber2) {
