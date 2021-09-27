0544cc21a021cfd07285125a833609b00e1777d4
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-09-27 13:11:04 +0300

    Change Title

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ var randomImageRight = "images/dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", randomImageRight);
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins !";
+}
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins !";
+}
+
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
